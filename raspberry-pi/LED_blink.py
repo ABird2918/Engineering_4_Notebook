e79a28c49734470225bf84a1a6e7e7b0efbda61e
ABird2918 <abird2918@charlottesvilleschools.org>
2023-09-06 10:25:23 -0400

    hgjf

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -1,13 +1,13 @@
-# type: ignore
-import time
-import board
-import digitalio
 
-led = digitalio.DigitalInOut(board.GP0)
+import time
+import board # type: ignore
+import digitalio # type: ignore
+
+led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
 while True:
-    led.value(True)
+    led.value = True
     time.sleep(1)
-    led.value(False)
+    led.value = False
     time.sleep(1)
