a1e92c00dab0b506f17d1872232eb6c5a9cf3592
ABird2918 <abird2918@charlottesvilleschools.org>
2023-09-07 10:24:39 -0400

    wetr

--- raspberry-pi/liftoff_countdown.py
+++ raspberry-pi/liftoff_countdown.py
@@ -2,9 +2,12 @@ import time
 import board #type: ignore
 import digitalio #type: ignore
 
-
+# for every integer between 10 and 0
+# print the countdown
+#go down by one every time
 for i in range(10,0,-1):
     print(i)
     time.sleep(1)
 
+# when the countdown reaches 0, print liftoff
 print("Liftoff!")
--- raspberry-pi/liftoff_lights.py
+++ raspberry-pi/liftoff_lights.py
@@ -1,12 +1,13 @@
 import time
 import board #type: ignore
 import digitalio #type: ignore
-
+# set up my leds
 greenled = digitalio.DigitalInOut(board.GP0)
 redled = digitalio.DigitalInOut(board.GP1)
 redled.direction = digitalio.Direction.OUTPUT
 greenled.direction = digitalio.Direction.OUTPUT
 
+# same countdown with a blinking red light
 for i in range(10,0,-1):
     print(i)
     redled.value = True
@@ -14,7 +15,7 @@ for i in range(10,0,-1):
     redled.value = False
     time.sleep(.5)
 
-
+# when the countdown finishes, print liftoff and turn led on
 print("Liftoff!")
 greenled.value = True
 time.sleep(10)
