8a589e8d9c3bfbc94b2d97a9e37b3f866be7793c
ABird2918 <abird2918@charlottesvilleschools.org>
2023-09-11 10:45:36 -0400

    sheth

--- raspberry-pi/LED_blink.py
+++ raspberry-pi/LED_blink.py
@@ -3,11 +3,16 @@ import time
 import board # type: ignore
 import digitalio # type: ignore
 
-led = digitalio.DigitalInOut(board.LED)
+led = digitalio.DigitalInOut(board.GP0)
 led.direction = digitalio.Direction.OUTPUT
 
+redled = digitalio.DigitalInOut(board.GP1)
+redled.direction = digitalio.Direction.OUTPUT
+
 while True:
+    redled.value = False
     led.value = True
     time.sleep(1)
     led.value = False
+    redled.value = True
     time.sleep(1)
---
+++ raspberry-pi/liftoff_button.py
@@ -0,0 +1,25 @@
+import time
+import board #type: ignore
+import digitalio #type: ignore
+# set up my leds and button
+greenled = digitalio.DigitalInOut(board.GP0)
+redled = digitalio.DigitalInOut(board.GP1)
+redled.direction = digitalio.Direction.OUTPUT
+greenled.direction = digitalio.Direction.OUTPUT
+button = digitalio.DigitalInOut(board.GP2)
+button.pull = digitalio.Pull.DOWN
+button.direction = digitalio.Direction.INPUT
+
+# same countdown with a blinking red light
+while True:
+    if (button.value): #If the button has been pushed:
+        for i in range(10,0,-1):
+            print(i)
+            redled.value = True
+            time.sleep(.5)
+            redled.value = False
+            time.sleep(.5)
+            # when the countdown finishes, print liftoff and turn led on
+        print("Liftoff!")
+        greenled.value = True
+        time.sleep(10)
