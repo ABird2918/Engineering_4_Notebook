edfa4d6423f755b14c43ffe1c1fd64e067d474c9
ABird2918 <abird2918@charlottesvilleschools.org>
2023-09-20 09:13:46 -0400

    adf

--- raspberry-pi/crashavoidance_part1.py
+++ raspberry-pi/crashavoidance_part1.py
@@ -5,12 +5,14 @@ import board
 import time
 import digitalio
 
+#set up pins to my accelerometer
 sda_pin = board.GP14
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin, sda_pin)
 
+#set the mpu value = to the input from the accelerometer
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
-while True:
+while True: # constantly print the accelerometer value
     print(mpu.acceleration)
-    time.sleep(.5)
+    time.sleep(.5) #twice per second
