e542f91b0af8cd66ff75bf81a2d023cb2766f154
ABird2918 <abird2918@charlottesvilleschools.org>
2024-01-04 10:43:22 -0500

    lkuyg

--- raspberry-pi/PITS.py
+++ raspberry-pi/PITS.py
@@ -12,7 +12,7 @@ import adafruit_lsm6dso32
 
 sda = board.GP4 #SDA pin
 scl = board.GP5 #SCL pin
-i2c = busio.I2C(scl,sda) #I2C device declaration
+#i2c = busio.I2C(scl,sda) #I2C device declaration
 sensor = adafruit_mpl3115a2.MPL3115A2(i2c) #identify what sensor i'm talking about
 
 #set up what values i can measure from the altimeter
@@ -23,7 +23,7 @@ temperature = sensor.temperature
 display_bus = displayio.I2CDisplay(i2c, device_address=0x3d, reset=board.GP2)
 display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
 
-mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68) #accelerometer
+#mpu = adafruit_mpu6050.MPU6050(i2c, address=0x68) #accelerometer
 while True: 
 # create the display group
 
@@ -32,4 +32,5 @@ while True:
     print(f"{sensor.altitude}")
     text_area = label.Label(terminalio.FONT, text=title, color=0xFFFF00, x=5, y=5)
     splash.append(text_area)    
-    text_area.text = f"{title}\n{round(mpu.gyro[0],3)}, {round(mpu.gyro[1],3)}, {round(mpu.gyro[2],3)}\n{sensor.altitude-altitude}"
+    text_area.text = f"{title}\n{round(mpu.gyro[0],3)}, {round(mpu.gyro[1],3)}, {round(mpu.gyro[2],3)}"
+    print(text_area.text)
