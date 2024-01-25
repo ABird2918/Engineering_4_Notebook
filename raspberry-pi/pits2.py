fcbdd0f023bad9d725b567df7e6bb0732559df21
ABird2918 <abird2918@charlottesvilleschools.org>
2024-01-25 10:47:52 -0500

    awe

--- raspberry-pi/pits2.py
+++ raspberry-pi/pits2.py
@@ -2,13 +2,15 @@
 import time
 import digitalio
 import board
-import lsm6dso32
+from adafruit_lsm6ds import lsm6dso32
 import busio
 
 sda = board.GP4 #SDA pin
 scl = board.GP5 #SCL pin
 i2c = busio.I2C(scl,sda) #I2C device declaration
 sensor = lsm6dso32(i2c)
+
+
 #altitude = sensor.altitude
 while True: 
 # create the display group
