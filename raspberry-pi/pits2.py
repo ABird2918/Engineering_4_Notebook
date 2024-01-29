deca88d86d76f0a337bf8a400a05634c2db4e007
ABird2918 <abird2918@charlottesvilleschools.org>
2024-01-29 10:40:58 -0500

    sdf

--- raspberry-pi/pits2.py
+++ raspberry-pi/pits2.py
@@ -2,13 +2,13 @@
 import time
 import digitalio
 import board
-from adafruit_lsm6ds import lsm6dso32
+from adafruit_lsm6ds.lsm6dso32 import LSM6DSO32
 import busio
 
 sda = board.GP4 #SDA pin
 scl = board.GP5 #SCL pin
 i2c = busio.I2C(scl,sda) #I2C device declaration
-sensor = lsm6dso32(i2c)
+sensor = LSM6DSO32(i2c)
 
 
 #altitude = sensor.altitude
@@ -16,5 +16,6 @@ while True:
 # create the display group
     
     #print(f"{sensor.altitude}")  
-    data = f"\n{round(sensor.gyro[0],3)}, {round(sensor.gyro[1],3)}, {round(sensor.gyro[2],3)}"
+    data = f"\n{round(sensor.acceleration[0],3)}, {round(sensor.acceleration[1],3)}, {round(sensor.acceleration[2],3)}"
     print(data)
+    time.sleep(2)
